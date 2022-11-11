0e76346c2ddb5b368a11bc38e78d268475ccd4bd
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-11 15:57:03 +0600

    added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+
+app.use(cors());
+app.use(express.json());
