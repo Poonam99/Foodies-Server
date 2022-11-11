845c18efed8b038ee7c552fb770f93866a6f637e
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-11 16:06:50 +0600

    mongodb conncted

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ require('dotenv').config();
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yi8iiuw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
 app.use(cors());
 app.use(express.json());
